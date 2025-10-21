8c2efcf6da66bd2942c3ec64b2a47eff0126cae7
Rahul <iamklrahul45@gmail.com>
2025-10-21 13:49:38 +0530

    Updated

--- src/Components/urlShortener/UrlShortener.jsx
+++ src/Components/urlShortener/UrlShortener.jsx
@@ -1,8 +1,6 @@
 import { useRef, useState } from "react";
 import { apiClient } from "../../shared/services/api-client.js";
 
-const BASE_URL=import.meta.env.VITE_BASE_URL;
-
 const UrlShortener = ({ compact = false }) => {
   const url = useRef();
   const [shortUrl, setShortURL] = useState("");
@@ -13,10 +11,8 @@ const UrlShortener = ({ compact = false }) => {
     const email=localStorage.getItem("email");
     try {
       const response = await apiClient.post("/short-url", { bigurl: URL, email });
-      if (response && response.data.shortid) {
-        console.log(response.data.shortid);
-        const fullShortUrl=`${BASE_URL}small/${response.data.shortid}`;
-        setShortURL(fullShortUrl);
+      if (response && response.data.shorturl) {
+        setShortURL(response.data.shorturl);
         setCopied(false);
       }
     } catch (err) {
